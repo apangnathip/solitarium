d19af4aa7075c8a6e505ee535545ce648f27dad4
apangnathip <alanpangnathip@gmail.com>
2026-04-17 18:25:17 -0400

    implement stacking logic

--- src/card.js
+++ src/card.js
@@ -1,9 +1,6 @@
 const CARD_WIDTH = 32;
 const CARD_HEIGHT = 48;
 const BOUNDS = { nw: { x: 6, y: 6 }, se: { x: 263, y: 243 } };
-const LAYER_BG = 1;
-const LAYER_MG = 2;
-const LAYER_FG = 3;
 
 /** @type {CardSystem} */
 let cardSystem;
@@ -14,7 +11,8 @@ class CardSystem {
     this.cols = 7;
     this.rows = 4;
     this.group = new Group();
-    this.cardObj = {};
+    this.spriteToCard = {};
+    this.groupToStack = {};
   }
 
   async init() {
@@ -24,54 +22,78 @@ class CardSystem {
   }
 
   layTableau() {
-    const px = CARD_WIDTH / 2 + 5;
     const py = CARD_HEIGHT / 2 + 5;
+    const px = CARD_WIDTH / 2 + 5;
     for (let i = 0; i < this.cols; i++) {
-      let stack = new this.group.Group();
-      let x = map(i, 0, this.cols - 1, BOUNDS.nw.x + px, BOUNDS.se.x - px);
+      const x = map(i, 0, this.cols - 1, BOUNDS.nw.x + px, BOUNDS.se.x - px);
+      let stack = new Stack(this, x, BOUNDS.nw.y + py);
       for (let j = 0; j < this.rows; j++) {
-        const y = BOUNDS.nw.y + py + 6 * j;
-        new CardObject(this, stack, x, y, j === this.rows - 1);
+        stack.newCard(j === this.rows - 1);
       }
     }
   }
 
-  /** @returns {CardObject} */
-  getObj(card) {
-    return this.cardObj[card.idNum];
+  /** @returns {Card} */
+  getWrapper(cardSprite) {
+    return this.spriteToCard[cardSprite.idNum];
   }
 
   update() {
     for (const sprite of this.group) {
-      cardSystem.getObj(sprite).update();
+      cardSystem.getWrapper(sprite).update();
     }
   }
 }
 
-class CardObject {
-  constructor(system, group, x, y, active = false) {
+class Card {
+  constructor(system, stack, x, y, active = false) {
     this.system = system;
-    this.group = group;
+    this.stack = stack;
+    this.active = active;
+    this.value = getRandomCard();
+    this.sprite = new stack.group.Sprite(
+      active ? this.value : "back",
+      x,
+      y,
+      DYNAMIC,
+    );
+    this.sprite.layer = stack.size();
+    this.sprite.overlaps(allSprites);
+    this.id = this.sprite.idNum;
+    this.system.spriteToCard[this.sprite.idNum] = this;
     this.fsm = new StateMachine();
     this.fsm
       .add("idle", new IdleState(this))
       .add("drag", new DragState(this))
       .add("hover", new HoverState(this))
       .add("reset", new ResetState(this))
+      .add("follow", new FollowState(this))
+      .add("flip", new FlipState(this))
       .change("idle");
+  }
 
-    /** @type {Sprite} */
-    this.sprite;
+  isDragging = () => this.sprite.mouse.dragging();
+  isHovering = () => this.sprite.mouse.hovering();
+  moveTowards = (...args) => this.sprite.moveTowards(...args);
 
-    if (active) {
-      this.sprite = new this.group.Sprite(getRandomCard(), x, y);
-      this.sprite.physics = KINEMATIC;
-    } else {
-      this.sprite = new this.group.Sprite("back", x, y);
-    }
+  update() {
+    this.fsm.update();
+  }
 
-    this.system.cardObj[this.sprite.idNum] = this;
-    this.sprite.layer = 1;
+  putOnTop(addon = 0) {
+    this.sprite.layer += 1000 + addon;
+  }
+
+  layer() {
+    return this.sprite.layer;
+  }
+
+  setLayer(layer) {
+    this.sprite.layer = layer;
+  }
+
+  getPos() {
+    return { x: this.sprite.x, y: this.sprite.y };
   }
 
   setPos({ x, y }) {
@@ -79,39 +101,135 @@ class CardObject {
     this.sprite.y = y;
   }
 
-  update() {
-    this.fsm.update();
+  splitStack() {
+    const sprites = this.stack.splitAt(this);
+    return sprites.map((sprite) => this.system.getWrapper(sprite));
+  }
+
+  flipUp() {
+    this.fsm.change("flip");
+  }
+}
+
+class Stack {
+  constructor(system, x, y) {
+    this.system = system;
+    this.group = new system.group.Group();
+    this.system.groupToStack[this.group.idNum] = this;
+    this.x = x;
+    this.y = y;
+    this.faceUp = 0;
+    this.faceDown = 0;
+    this.gap = 9;
+    this.backGap = 4;
+  }
+
+  isOverlapping = (...args) => this.group.overlapping(...args);
+
+  newCard(isActive) {
+    const { x, y } = this.getTopPos();
+    new Card(this.system, this, x, y, isActive);
+    isActive ? this.faceUp++ : this.faceDown++;
+  }
+
+  size() {
+    return this.faceDown + this.faceUp;
+  }
+
+  pop() {
+    let card = this.group.pop();
+    if (!card) return;
+    this.faceUp--;
+    return card;
+  }
+
+  popTo(card) {
+    let cardSprite;
+    while ((cardSprite = this.group.pop())) {
+      this.faceUp--;
+      if (cardSprite.idNum === card.id) return;
+    }
+  }
+
+  push(...cards) {
+    for (const card of cards) {
+      this.group.push(card.sprite);
+      this.faceUp++;
+    }
+  }
+
+  getTopPos(ignoreTop = 0) {
+    return {
+      x: this.x,
+      y:
+        this.y +
+        this.gap * (this.faceUp - ignoreTop) +
+        this.backGap * this.faceDown,
+    };
+  }
+
+  getTopCard() {
+    return this.system.getWrapper(this.group.at(-1));
+  }
+
+  splitAt(card) {
+    return this.group.slice(this.getCardIdx(card));
+  }
+
+  getCardIdx(card) {
+    return this.group.findIndex(({ idNum }) => card.id === idNum);
+  }
+
+  updateCardLayers() {
+    for (const [i, sprite] of this.group.entries()) {
+      sprite.layer = i + 1;
+    }
+  }
+
+  flipTopCard() {
+    if (this.faceUp || !this.size()) return;
+    this.getTopCard().flipUp();
+    this.faceUp++;
+    this.faceDown--;
+  }
+
+  isLegalPush(card) {
+    return checkStackLegality(card.value, this.getTopCard().value);
   }
 }
 
 class CardState extends State {
-  /** @param {CardObject} cardObj  */
-  constructor(cardObj) {
-    super(cardObj.fsm);
-    this.card = cardObj;
+  /** @param {Card} card  */
+  constructor(card) {
+    super(card.fsm);
+    this.card = card;
+    this.system = card.system;
   }
 }
 
 class IdleState extends CardState {
   update() {
-    if (this.card.sprite.mouse.dragging()) this.fsm.change("drag");
-    if (this.card.sprite.mouse.hovering()) this.fsm.change("hover");
+    if (!this.card.active) return;
+    if (this.card.isDragging()) this.fsm.change("drag");
+    if (this.card.isHovering()) this.fsm.change("hover");
   }
 }
 
 class HoverState extends CardState {
   update() {
-    if (!this.card.sprite.mouse.hovering()) {
+    if (!this.card.isHovering()) {
       this.fsm.change("idle");
       return;
     }
-    if (this.card.sprite.mouse.dragging()) {
+    if (this.card.isDragging()) {
       this.fsm.change("drag");
       return;
     }
 
-    this.card.sprite.x = this.clampedPull(this.startPos.x, mouse.x);
-    this.card.sprite.y = this.clampedPull(this.startPos.y, mouse.y);
+    this.card.setPos({
+      x: this.clampedPull(this.startPos.x, mouse.x),
+      y: this.clampedPull(this.startPos.y, mouse.y),
+    });
   }
 
   enter() {
@@ -119,7 +237,7 @@ class HoverState extends CardState {
   }
 
   exit() {
-    this.card.setPos(this.startPos);
+    this.card.sprite.moveTo(this.startPos);
   }
 
   clampedPull(from, to) {
@@ -129,25 +247,72 @@ class HoverState extends CardState {
 
 class DragState extends CardState {
   update() {
-    if (!this.card.sprite.mouse.dragging()) {
-      this.fsm.change("reset", this.startPos);
+    if (!this.card.isDragging()) {
+      this.changeStack();
+      return;
     }
-    this.card.sprite.moveTowards(mouse, 0.4);
+    this.detectStackHover();
+    this.dragCards();
+  }
+
+  dragCards() {
+    this.cards.forEach((card, i) => {
+      if (card === this.card) {
+        card.moveTowards(vecSub(this.grabOffset, mouse), 1);
+        return;
+      }
+      card.fsm.change(
+        "follow",
+        {
+          x: this.grabOffset.x,
+          y: this.grabOffset.y,
+        },
+        i,
+      );
+    });
+  }
+
+  detectStackHover() {
+    for (const key in this.system.groupToStack) {
+      const stack = this.system.groupToStack[key];
+      const isClose = Math.abs(this.card.getPos().x - stack.x) < CARD_WIDTH / 2;
+      if (stack.isOverlapping(this.card.sprite) && isClose) {
+        this.newStack = stack;
+      }
+    }
+  }
+
+  changeStack() {
+    if (!this.newStack.isLegalPush(this.card)) {
+      this.newStack = this.oldStack;
+    }
+    if (this.newStack !== this.card.stack) {
+      this.card.stack.popTo(this.card);
+      this.newStack.push(...this.cards);
+    }
+    this.cards.forEach((card, i) => {
+      card.stack = this.newStack;
+      card.fsm.change("reset", this.newStack.getTopPos(this.cards.length - i));
+    });
   }
 
   enter() {
-    this.card.sprite.layer++;
-    this.startPos = { x: this.card.sprite.x, y: this.card.sprite.y };
+    this.oldStack = this.card.stack;
+    this.newStack = this.card.stack;
+    this.grabOffset = vecSub(this.card.getPos(), mouse);
+    this.cards = this.card.splitStack();
+    for (const card of this.cards) card.putOnTop();
   }
 
   exit() {
-    this.card.sprite.layer--;
+    this.newStack.updateCardLayers();
+    this.oldStack.flipTopCard();
   }
 }
 
 class ResetState extends CardState {
   update() {
-    this.card.sprite.moveTowards(this.startPos, 0.6);
+    this.card.moveTowards(this.startPos, 0.5);
 
     if (!this.card.sprite.isMoving) {
       this.fsm.change("idle");
@@ -156,11 +321,40 @@ class ResetState extends CardState {
   }
 
   enter(startPos) {
-    this.card.sprite.layer++;
     this.startPos = startPos;
   }
+}
 
-  exit() {
-    this.card.sprite.layer--;
+class FollowState extends CardState {
+  update() {
+    this.card.moveTowards(vecSub(this.offset, mouse), 1 / (this.idx + 1));
+  }
+
+  enter(offset, idx) {
+    this.idx = idx;
+    this.offset = offset;
+    this.offset.y -= this.card.stack.gap * idx;
+  }
+}
+
+class FlipState extends CardState {
+  update() {
+    this.card.sprite.scale.x -= this.speed * (deltaTime / 1000);
+
+    if (this.card.sprite.scale.x < 0) {
+      this.card.sprite.scale.x = 0;
+      this.card.sprite.changeAni(this.card.value);
+      this.speed *= -1;
+    }
+
+    if (this.card.sprite.scale.x >= 1) {
+      this.card.sprite.scale.x = 1;
+      this.card.fsm.change("idle");
+      this.card.active = true;
+    }
+  }
+
+  enter() {
+    this.speed = 20;
   }
 }
--- src/helper.js
+++ src/helper.js
@@ -2,10 +2,60 @@ function clamp(num, min, max) {
   return Math.max(min, Math.min(num, max));
 }
 
+function vecSub(v1, v2) {
+  return { x: v2.x - v1.x, y: v2.y - v1.y };
+}
+
 function getRandomCard() {
   const rs = ["2", "3", "4", "5", "6", "7", "8", "9", "0", "J", "Q", "K", "A"];
   const ss = ["H", "D", "C", "S"];
   const r = rs[Math.floor(Math.random() * rs.length)];
   const s = ss[Math.floor(Math.random() * ss.length)];
   return r + s;
+}
+
+function splitValue(value) {
+  return {
+    rank: value[0],
+    suit: value[1],
+  };
+}
+
+function isSameColor(suit1, suit2) {
+  const red = new Set(["H", "D"]);
+  const black = new Set(["C", "S"]);
+  return (
+    (red.has(suit1) && red.has(suit2)) || (black.has(suit1) && black.has(suit2))
+  );
+}
+
+function rankToNumeric(rank) {
+  const dict = {
+    A: 1,
+    2: 2,
+    3: 3,
+    4: 4,
+    5: 5,
+    6: 6,
+    7: 7,
+    8: 8,
+    9: 9,
+    0: 10,
+    J: 11,
+    Q: 12,
+    K: 13,
+  };
+  return dict[rank];
+}
+
+function isLowerByOne(rank1, rank2) {
+  return rankToNumeric(rank2) - rankToNumeric(rank1) === 1;
+}
+
+function checkStackLegality(value1, value2) {
+  const a = splitValue(value1);
+  const b = splitValue(value2);
+  if (isSameColor(a.suit, b.suit)) return false;
+  if (!isLowerByOne(a.rank, b.rank)) return false;
+  return true;
 }
